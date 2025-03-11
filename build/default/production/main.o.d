d9a75db5f425d81f609dd00dd7148b15eeb5c6b2
‪Vladimir Lihatchov‬‏ <lichvladimir@gmail.com>
2025-03-11 13:23:44 +0200

    build no error

---
+++ build/default/production/main.o.d
@@ -0,0 +1,15 @@
+build/default/production/main.o: main.c System/system.h System/delay.h \
+ oledDriver/oledC.h oledDriver/oledC_colors.h oledDriver/oledC_shapes.h \
+ i2cDriver/i2c1_driver.h
+
+System/system.h:
+
+System/delay.h:
+
+oledDriver/oledC.h:
+
+oledDriver/oledC_colors.h:
+
+oledDriver/oledC_shapes.h:
+
+i2cDriver/i2c1_driver.h:
